95b0409c79df3f38e4b5c011fa64e09b7f72fdb3
ANDRES CRUZ Y VERA <acruzyve@itam.mx>
2018-11-12 20:55:14 -0600

    Ej4 funciona

--- Ej_4.m
+++ Ej_4.m
@@ -6,14 +6,16 @@ t0 = 0;
 T = 10;
 n = 50;
 
+%Euler implicito
 %n=50 => h = 1/5
 [w t] = eulerImp(y0, t0,T, n);
 %dobleu = w(10)
-%w(10) es -0.0054
+%w(10) es 5.000
 %Converge a la sol estacionario y=0
 %plot(t,w)
 hold on
 
+%Euler explicito
 n=200;
 %n = 200 => h = 1/20
 [w,t] = eulerExp(f,y0,n,t0,T);
@@ -29,18 +31,11 @@ n=100;
 [w,t] = eulerExp(f,y0,n,t0,T);
 %plot(t,w)
 
-n=200;
-%n = 200 => h = 1/20
-[w,t] = eulerExp(f,y0,n,t0,T);
-%plot(t,w)
 
-tspan = [0 10];
-[t,y] = ode45(f, tspan, y0);
-%plot(t,y)
 
 %RK23
-n=90;
-tol  = 0.1;
-h0 =0.2;
+n=180;
+tol  = 0.001;
+h0 =0.002;
 [w,t] = rk23(f,y0,t0,n,tol,h0);
 plot(t,w)
--- Newton.m
+++ Newton.m
@@ -1,5 +1,6 @@
-function [a] = Newton (f, fPrima,a, tol)
-  while (f(a)+a) > tol
-    a = a - (f(a)+a)/fPrima(a);
+function [a] = Newton (f, fPrima,x0, tol)
+    a = x0;
+  while f(a) > tol
+    a = a - f(a)/fPrima(a);
   end
-endfunction
+end
